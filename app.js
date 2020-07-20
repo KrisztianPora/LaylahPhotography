cb14f999a29e829cd824c8d71e0276d42d7d21eb
Krisztián Póra <krisztian.pora@gmail.com>
2020-07-20 15:31:13 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const popup = document.querySelector(".popup");
+const previews = document.querySelectorAll(".gallery img");
+const original = document.querySelector(".full-img");
+
+previews.forEach((preview) => {
+  preview.addEventListener("click", () => {
+    popup.classList.add("open");
+    original.classList.add("open");
+
+    const originalSrc = preview.getAttribute("data-original");
+    original.src = `./img/${originalSrc}`;
+  });
+});
+
+popup.addEventListener("click", (e) => {
+  if (e.target.classList.contains("popup")) {
+    popup.classList.remove("open");
+    original.classList.remove("open");
+  }
+});
